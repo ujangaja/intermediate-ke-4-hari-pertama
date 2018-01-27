c7f911d6aca228a636324833dd96673284de7ed6
Ujang Arisandi <ujangaja@gmail.com>
2018-01-27 11:55:20 +0700

    source

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ intermediate ke-4  hari pertama fasilitator mas edi
   2.buat file koneksi dengan nama dbConnect.php
   3.buat file produk.php
 
+
   4.membuat ApiClient pada javaclass
+
+  5.tambahkan GsonConverterFactory     compile 'com.squareup.retrofit2:converter-gson:2.1.0'
+
+
--- app/src/main/java/com/example/ujang/crud/ApiClient.java
+++ app/src/main/java/com/example/ujang/crud/ApiClient.java
@@ -1,8 +1,21 @@
 package com.example.ujang.crud;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by ujang on 1/27/2018.
  */
 
 public class ApiClient {
+
+    public static final String BASE_URL = "";
+
+    public static Retrofit getRetrofit(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
 }
