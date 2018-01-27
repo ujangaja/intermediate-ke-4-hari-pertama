44102481ee12be47b68c5ec2a4fe82a2d03f82f6
Ujang Arisandi <ujangaja@gmail.com>
2018-01-27 12:45:43 +0700

    membuat strukture model

---
+++ app/src/main/java/com/example/ujang/crud/Produk.java
@@ -0,0 +1,46 @@
+package com.example.ujang.crud;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by ujang on 1/27/2018.
+ */
+
+public class Produk {
+
+    @SerializedName("id")
+    String Id;
+    @SerializedName("nama_produk")
+    String nama;
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public String getStok() {
+        return stok;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    @SerializedName("harga")
+
+    String harga;
+    @SerializedName("stok")
+    String stok;
+    @SerializedName("gambar")
+    String gambar;
+
+
+
+}
