f256df00351bfe2a9df7a119732bba611d25fdeb
Ujang Arisandi <ujangaja@gmail.com>
2018-01-27 13:47:20 +0700

    ApiInterface

--- app/src/main/java/com/example/ujang/crud/ApiInterface.java
+++ app/src/main/java/com/example/ujang/crud/ApiInterface.java
@@ -1,8 +1,16 @@
 package com.example.ujang.crud;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by ujang on 1/27/2018.
  */
 
 public interface ApiInterface {
+    @GET("produk.php")
+    Call<List<Produk>>ambilProduk();
+
 }
--- app/src/main/java/com/example/ujang/crud/BelajarAndroid.java
+++ app/src/main/java/com/example/ujang/crud/BelajarAndroid.java
@@ -7,6 +7,10 @@ import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class BelajarAndroid extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.LayoutManager manager;
@@ -25,5 +29,24 @@ public class BelajarAndroid extends AppCompatActivity {
     }
 
     private void loadProduk() {
+        ApiInterface api = ApiClient.getRetrofit()
+                .create(ApiInterface.class);
+
+        Call<List<Produk>> call = api.ambilProduk();
+        call.enqueue(new Callback<List<Produk>>() {
+
+
+            @Override
+            public void onResponse(Call<List<Produk>> call, Response<List<Produk>> response) {
+                produks = response.body();
+                adapter = new ProdukAdapter(produks,BelajarAndroid.this);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Produk>> call, Throwable t) {
+
+            }
+        });
     }
 }
