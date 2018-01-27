9f9e5142c5df98ef039ca26034cf6eec2cb5fc1e
Ujang Arisandi <ujangaja@gmail.com>
2018-01-27 10:53:57 +0700

    instalasi android studio

---
+++ app/src/main/java/com/example/ujang/crud/BelajarAndroid.java
@@ -0,0 +1,13 @@
+package com.example.ujang.crud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BelajarAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belajar_android);
+    }
+}
