2cbb9a3f4c2878d09df5a9773782771a1de0c003
Ujang Arisandi <ujangaja@gmail.com>
2018-02-10 16:10:42 +0700

    menambahakan data baru menu_detail

--- app/src/main/java/com/example/ujang/crud/DetailProduk.java
+++ app/src/main/java/com/example/ujang/crud/DetailProduk.java
@@ -1,9 +1,13 @@
 package com.example.ujang.crud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.GsonBuilder;
 import com.squareup.picasso.Picasso;
@@ -17,6 +21,7 @@ public class DetailProduk extends AppCompatActivity {
     @BindView(R.id.nama)TextView Nama;
     @BindView(R.id.harga)TextView Harga;
     @BindView(R.id.stok)TextView Stok;
+    Produk prods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +30,38 @@ public class DetailProduk extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        Produk prods = new GsonBuilder().create().fromJson(getIntent().getStringExtra("produk"),Produk.class);
+         prods = new GsonBuilder().create().fromJson(getIntent().getStringExtra("produk"),Produk.class);
         Nama.setText(prods.getNama());
         Harga.setText(prods.getHarga());
         Stok.setText(prods.getStok());
         Picasso.with(DetailProduk.this).load("http://192.168.243.1/iak/"+prods.getGambar()).into(Gambar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail,menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.edit:
+                Intent i = new Intent(DetailProduk.this, EditProduk.class);
+                i.putExtra("nama", prods.getNama());
+                i.putExtra("harga", prods.getHarga());
+                i.putExtra("stok", prods.getStok());
+                i.putExtra("gambar", prods.getGambar());
+                i.putExtra("id", prods.getId());
+                startActivity(i);
+                Toast.makeText(this, "Edit", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/ujang/crud/EditProduk.java
+++ app/src/main/java/com/example/ujang/crud/EditProduk.java
@@ -2,12 +2,22 @@ package com.example.ujang.crud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EditProduk extends AppCompatActivity {
+    @BindView(R.id.submit)Button Submit;
+    @BindView(R.id.nama)EditText nama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_produk);
+
+        ButterKnife.bind(this);
+
     }
 }
